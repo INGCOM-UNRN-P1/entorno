cbbf52fce81a97bf5aa368ea1d68ad97f8bda3fa
Martin Rene <martinvilu@gmail.com>
2026-06-20 00:59:40 -0300

    chore: clean up test files again

--- test.ps1
+++
@@ -1,3 +0,0 @@
-$str = "MSYSTEM = `"CLANG64`","
-$str2 = $str -replace 'MSYSTEM\s*=\s*"CLANG64",', "`$0`r`n  MSYS2_PATH_TYPE = `"inherit`","
-Write-Host "Result: $str2"
--- wezterm.lua
+++
@@ -1,7 +0,0 @@
-config.set_environment_variables = {
-  MSYSTEM = "CLANG64",
-  CHERE_INVOKING = "1",
-  HOME = home_dir,
-  PATH = custom_path,
-  LANG = "es_AR.UTF-8",
-}
